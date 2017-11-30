af70a27e04bf1e3b7faad9e535ea59a53035eff5
Toto <rometpierre@gmail.com>
2017-11-30 19:27:46 +0100

    add ENUM for position

--- Project_EV3/src/network/SendServer.java
+++ Project_EV3/src/network/SendServer.java
@@ -17,7 +17,7 @@ public class SendServer {
 	public void dataToSend(int pID, int pPosition, int pSpeed)
 	{
 		try {
-			CentralizedSync.sendPos( Integer.toString(pID) + "\r\n" + Integer.toString(pPosition) + "\r\n" +  Integer.toString(pSpeed) + "\r\n" );
+			CentralizedSync.sendPos( Integer.toString(pID) + "\r\n" + Integer.toString(pPosition) + "\r\n" +  Integer.toString(pSpeed) );
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
