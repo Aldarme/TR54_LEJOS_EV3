c684e9491ac663ec0afe445cd901285d0f4c09fe
LO52_Comateux_Numérique <vavaburger@gmail.com>
2017-12-06 10:25:59 +0100

    Change sequence to requestSequence, add time to robot data

--- Project_EV3/src/network/ReceiveServer.java
+++ Project_EV3/src/network/ReceiveServer.java
@@ -15,9 +15,10 @@ public class ReceiveServer {
 	
 	static String data;
 	static int rId, rLocation, rSpeed;
+	static double rTime;
 	static boolean found = false;
 	
-	static ArrayList<RobotData> sequence = new ArrayList<RobotData>();
+	static ArrayList<RobotData> requestSequence = new ArrayList<RobotData>();
 	
 	public static void main(String[] args) throws SocketException {
 		r = BroadcastReceiver.getInstance();
@@ -42,11 +43,14 @@ public class ReceiveServer {
 	{
 		String delim = "\r\n";
 		
+		//Get the values from the data received
 		String[] values = data.split(delim);
 		
 		rId = Integer.parseInt(values[0]);
 		rLocation = Integer.parseInt(values[1]);
 		rSpeed = Integer.parseInt(values[2]);
+		//Register system time when data are processed
+		rTime = System.nanoTime();
 		
 		updateSequence();
 	}
@@ -54,30 +58,36 @@ public class ReceiveServer {
 	private static void updateSequence()
 	{			
 		//TODO
-		// Enlever les robots qui sortent de la zone de conflit de la séquence de passage
+		// Finir : Enlever les robots qui sortent de la zone de conflit de la séquence de passage
 		
+		int i = 0;
 		
 		// Go through the sequence list
-		for (RobotData r : sequence) {
+		for (RobotData r : requestSequence) {
+			i++;
 			// If the Robot is on the sequence list
-			 if (r.getId() == rId) {
-				 // Refresh it's location and speed
-				 r.setLocation(rLocation);
-				 r.setSpeed(rSpeed);
+			if (r.getId() == rId) {
+				/*if(rLocation == "ZONE DE SORTIE") {
+					 requestSequence.remove(i);
+				}else */
+					 // Refresh it's location and speed
+					 r.setLocation(rLocation);
+					 r.setSpeed(rSpeed);
+			 	//}
 				 
-				 //Set the found flag
-				 found = true;
-			 }
+				//Set the found flag
+				found = true;
+			}
 		}
 		
 		// If the Robot is not on the sequence list
 		if(!found) {
 			// Add the Robot to the sequence
-			sequence.add(new RobotData(rId, rLocation, rSpeed));
-			
-			// Reset the flag
-			found = false;
+			requestSequence.add(new RobotData(rId, rLocation, rSpeed, rTime));
 		}
+		
+		// Reset the flag
+		found = false;
+		
 	}
-
 }
--- Project_EV3/src/robotmodel/RobotData.java
+++ Project_EV3/src/robotmodel/RobotData.java
@@ -4,12 +4,14 @@ public class RobotData {
 	private int id;
 	private int location;
 	private int speed;
+	private double time;
 	
-	public RobotData(int id, int location, int speed)
+	public RobotData(int id, int location, int speed, double time)
 	{
 		this.id = id;
 		this.location = location;
 		this.speed = speed;
+		this.time = time;
 	}
 	
 	public int getId() {
@@ -32,4 +34,11 @@ public class RobotData {
 		this.speed = speed;
 	}
 	
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
 }
