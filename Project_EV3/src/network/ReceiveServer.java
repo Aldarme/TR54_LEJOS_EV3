c0d1dd901034a671d7fe50e95edffbb082978c56
LO52_Comateux_Numérique <vavaburger@gmail.com>
2017-12-13 15:57:52 +0100

    Update Request Sequence and add passage Sequence (TODO : Finish Passage)

--- Project_EV3/src/network/ReceiveServer.java
+++ Project_EV3/src/network/ReceiveServer.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import lejos.hardware.lcd.LCD;
 import lejos.utility.Delay;
 import network.BroadcastReceiver;
+import robotmodel.Position;
 import robotmodel.RobotData;
 
 public class ReceiveServer {
@@ -15,10 +16,12 @@ public class ReceiveServer {
 	
 	static String data;
 	static int rId, rLocation, rSpeed;
+	static int deltaTime = 5;
 	static double rTime;
 	static boolean found = false;
 	
 	static ArrayList<RobotData> requestSequence = new ArrayList<RobotData>();
+	static ArrayList<RobotData> passageSequence = new ArrayList<RobotData>();
 	
 	public static void main(String[] args) throws SocketException {
 		r = BroadcastReceiver.getInstance();
@@ -31,7 +34,7 @@ public class ReceiveServer {
 			// Get the data from the robot
 			data = l.getData();
 			
-			// If there are some data
+			// If there are some new data
 			if (data != null){
 				LCD.drawString(String.format("Data : ", data), 0, 3);
 				processData();
@@ -50,15 +53,13 @@ public class ReceiveServer {
 		rLocation = Integer.parseInt(values[1]);
 		rSpeed = Integer.parseInt(values[2]);
 		//Register system time when data are processed
-		rTime = System.nanoTime();
+		rTime = System.nanoTime()*Math.pow(10,-9);
 		
-		updateSequence();
+		updateRequestSequence();
 	}
 	
-	private static void updateSequence()
+	private static void updateRequestSequence()
 	{			
-		//TODO
-		// Finir : Enlever les robots qui sortent de la zone de conflit de la séquence de passage
 		
 		int i = 0;
 		
@@ -67,17 +68,13 @@ public class ReceiveServer {
 			i++;
 			// If the Robot is on the sequence list
 			if (r.getId() == rId) {
-				/*if(rLocation == "ZONE DE SORTIE") {
-					 requestSequence.remove(i);
-				}else */
-					 // Refresh it's location and speed
-					 r.setLocation(rLocation);
-					 r.setSpeed(rSpeed);
-			 	//}
-				 
+				// Refresh it's location and speed
+				r.setLocation(rLocation);
+				r.setSpeed(rSpeed);
+			}
+				
 				//Set the found flag
 				found = true;
-			}
 		}
 		
 		// If the Robot is not on the sequence list
@@ -88,6 +85,59 @@ public class ReceiveServer {
 		
 		// Reset the flag
 		found = false;
+		
+		updatePassageSequence();
+		
+	}
+	
+	private static void updatePassageSequence()
+	{
+		int i = 0;
+		
+		if(requestSequence.size() == 1 && passageSequence.isEmpty())
+		{
+			passageSequence.add(requestSequence.get(0));
+		}else {
+			for (RobotData r : requestSequence) {
+				i++;
+				for(int o=0;o<passageSequence.size();o++)
+				{
+					// If new data from robot already in passageSequence -> Update
+					if(r.getId() == passageSequence.get(o).getId())
+					{
+						if(Position.SORTIE.equals(r.getLocation()))
+						{
+							passageSequence.remove(o);
+							requestSequence.remove(i);
+						}else {
+							passageSequence.get(o).setLocation(r.getLocation());
+							passageSequence.get(o).setSpeed(r.getSpeed());
+						}
+						found = true;
+					}
+				}
+				if(!found)
+				{
+					//TODO
+					// Add getRL() function which return true or false depending on which path he is on.
+					
+					// If the Robot from the request list is from the same path as the last one from sequence list, add the Robot
+					/*if(passageSequence.get(passageSequence.size()-1).getRL() == r.getRL())
+					{
+						passageSequence.add(r);
+					}else {
+						if(r.getTime()-passageSequence.get(passageSequence.size()-1).getTime() >= deltaTime)
+						{
+							passageSequence.add(r);
+						}
+					}*/
+				}
+			}
+			found = false;
+		}
+		
+		//TODO
+		// Send the passage sequence to all the robot
 		
 	}
 }
