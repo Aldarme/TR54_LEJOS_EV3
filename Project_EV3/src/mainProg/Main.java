66b2c035ee2bbf8fda737ceb38994f92d79b4260
Toto <rometpierre@gmail.com>
2017-12-14 14:08:00 +0100

    add thread synchronisation for orange mark comportment

--- Project_EV3/src/mainProg/Main.java
+++ Project_EV3/src/mainProg/Main.java
@@ -11,7 +11,7 @@ import threads.*;
 
 public class Main {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		//Init my robot with an ID
 		Robot myRobot = new Robot(0);
@@ -37,13 +37,14 @@ public class Main {
 			/*
 			 * Orange mark detection
 			 */
-			if (myRobot.getColorSensor()[0] == orangeTab[0]
-				&& myRobot.getColorSensor()[1] == orangeTab[1]
+			if (myRobot.getColorSensor()[0] == orangeTab[0]		//get real value
+				&& myRobot.getColorSensor()[1] == orangeTab[1]	//of ORANGE RGB
 				&&myRobot.getColorSensor()[2] == orangeTab[2]
 				)
 			{
 				//Start the Thread for Entree Zone
 				threadEntree.start();
+				threadEntree.join();
 				
 				if(myRobot.getValidServer() == false)
 				{
@@ -51,16 +52,19 @@ public class Main {
 					
 					//Start the Thread for Stock Zone
 					threadStock.start();
+					threadStock.join();
 					
 					//Wait that "ValidServer" was update from false to true
 					while(myRobot.getValidServer() == false){}					
 				}
 				
 				//Start the Thread for Conflict Zone
-				threadConflict.start();
+				threadConflict.start();				
+				threadConflict.join();
 				
 				//Start the Thread for Sortie Zone
-				threadSortie.start();				
+				threadSortie.start();	
+				threadSortie.join();
 			}
 		}
 	}
